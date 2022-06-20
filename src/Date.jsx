f12a85fbf08a6d6d0121645fc5bf889250ea669e
Mamun13 <mamuncse82@gmail.com>
2022-06-20 09:45:52 +0600

    Digital_clock

---
+++ src/Date.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Digiclock=()=>{
+    let date = new Date().toLocaleDateString();
+
+    return(
+        <>
+            <p className="date">{date}</p>
+        </>
+        
+    )
+}
+export default Digiclock;
